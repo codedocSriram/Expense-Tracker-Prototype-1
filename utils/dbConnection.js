d5d426923a025f0c2564ebd48c73185c34c4886e
Sriram Annadurai <42068041+codedocSriram@users.noreply.github.com>
2025-11-26 05:24:09 +0530

    Change dbHost to use DB_ENDPOINT environment variable

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
+const dbHost = process.env.DB_ENDPOINT;
 const dbDialect = process.env.DB_DIALECT;
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
@@ -20,6 +20,7 @@ const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
 })();
 
 module.exports = sequelize;
+
 
 
 
